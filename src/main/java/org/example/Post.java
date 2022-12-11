3c05d63cdfca54e973682b9b49c55e0abd8a66c6
Anastasiya Kobeleva <anastasiya.kobeleva.95@gmail.com>
2022-12-11 11:34:00 +0300

    first commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Post {
+    FormDate formDate = new FormDate();
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
